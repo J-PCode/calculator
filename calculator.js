bbe24ac2c49b80a5bdfa89330f83fffd449c3ca3
J-PCode <89179060+J-PCode@users.noreply.github.com>
2022-01-27 06:24:20 +0200

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const taul =[]; // values [num, num, operand]
+function add(taul){
+    return tulos = taul[0] + taul[1]
+    }
+function subtract(taul){
+    return tulos = taul[0] - taul[1];
+}
+function multiply(taul){
+    return tulos = taul[0] / taul[1];
+}
+function divide(taul){
+    return tulos = taul[0] / taul[1];
+}
+
+//operateen taitaa tulla eventlistenerit ym
+function operate(taul){
+    taul[2] = operator //addEventListener()
+    taul[0] = "value1"
+    taul[1] = "value2"
+
+    //voisiko tässä käyttää reducea? sehän laskee viimeisimmän arvon ja nykyisen arvon
+}
